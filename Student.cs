79b1933370013c9c3b32dd53d22d729b93f619d5
Arrey Tabe Ebob <60329084+TABEEBOB@users.noreply.github.com>
2020-08-15 18:51:05 -0400

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,57 @@
+﻿
+/****************
+ * Michael Arrey
+ * C#
+ * 04/02/2020
+ * *************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject
+{
+    class Student : Person
+    {
+      //global variables.
+        
+        private string major;
+        private string gpa;
+
+        public Student()//no argument constructor. set all variables to empty string
+        {
+            name = "";
+            address = "";
+            phone = "";
+            email = "";
+            major = "";
+            gpa = "";
+
+        }
+        //constructor with 6 argument
+        public Student(string name, string address, string phone, string email, string major, string gpa)
+        {
+            this.name = name;
+            this. address = address;
+            this.phone = phone;
+            this.email = email;
+            this. major = major;
+            this.gpa = gpa;
+
+        }
+
+        //getter and setter for all variables.
+        
+        public string getMajor() { return major; }
+        public void setMajor(string major) {this.major = major; }
+
+        public string getGPA() { return gpa; }
+        public void setGPA(string gpa) {this.gpa = gpa; }
+
+        //display() method
+        
+
+
+    }
+}
