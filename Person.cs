d3a83ffdba92217955bdddd36314441303353ceb
Arrey Tabe Ebob <60329084+TABEEBOB@users.noreply.github.com>
2020-08-15 18:53:50 -0400

    Add files via upload
    person class is a super class that had the same attributes as student

---
+++ Person.cs
@@ -0,0 +1,64 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject
+{
+    class Person
+    {
+        //class variables
+        public String name;
+        public String address;
+        public String phone;
+        public String email;
+
+        //getters and setters for class variables
+        public void setName(String name) { 
+
+            this.name = name; 
+        }
+        public String getName() { 
+
+            return name; 
+        }
+
+        public void setAddress(String address) { 
+            
+            this.address = address; 
+        }
+        public String getAddress() {
+            
+            return address; 
+        }
+
+        public void setPhone(String phone) { 
+            
+            this.phone = phone; 
+        }
+        public String getPhone() { 
+            
+            return phone;
+        }
+
+        public void setEmail(String email) { 
+            
+            this.email = email; 
+        }
+        public String getEmail() { 
+            
+            return email; 
+        }
+
+        public virtual void Display()//make display method virtual
+        {
+            Console.WriteLine("Student Name=" + getName());
+            Console.WriteLine("Address=" + getAddress());
+            Console.WriteLine("Phone=" + getPhone());
+            Console.WriteLine("Email=" + getEmail());
+        }
+    }
+}
